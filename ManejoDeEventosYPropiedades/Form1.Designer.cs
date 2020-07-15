4c55a54ae71c6fbb0b4a22ef2b13eff6af4063dc
collymoore2 <68247899+collymoore2@users.noreply.github.com>
2020-07-15 10:30:14 -0700

    evento1
    collymoore c sharp

---
+++ ManejoDeEventosYPropiedades/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace ManejoDeEventosYPropiedades
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.lstbContenedorItems = new System.Windows.Forms.ListBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Location = new System.Drawing.Point(51, 66);
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(100, 20);
+            this.txtTexto.TabIndex = 0;
+            // 
+            // lstbContenedorItems
+            // 
+            this.lstbContenedorItems.FormattingEnabled = true;
+            this.lstbContenedorItems.Location = new System.Drawing.Point(51, 92);
+            this.lstbContenedorItems.Name = "lstbContenedorItems";
+            this.lstbContenedorItems.Size = new System.Drawing.Size(181, 82);
+            this.lstbContenedorItems.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(157, 63);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Ingresar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.lstbContenedorItems);
+            this.Controls.Add(this.txtTexto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTexto;
+        private System.Windows.Forms.ListBox lstbContenedorItems;
+        private System.Windows.Forms.Button btnAgregar;
+    }
+}
+
